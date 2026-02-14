4dce5059c0c1642e39ec5ba6027cda2450aa4b59
BansaLROHanHi <166013511+BansaLROHanHi@users.noreply.github.com>
2026-02-14 23:33:07 +0530

    Delete backend/create_tables.py
    Not required

--- backend/create_tables.py
+++
@@ -1,9 +0,0 @@
-from app.db.session import engine
-from app.db.base import Base
-
-import app.models.well
-import app.models.curve
-
-print("Creating tables...")
-Base.metadata.create_all(bind=engine)
-print("Done.")
